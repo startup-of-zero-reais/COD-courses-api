427bf96572f52024bd9377c749c1c2f9ec9a28ea
Jean <jean.carlosmolossi@gmail.com>
2021-12-15 00:05:08 -0300

    refactor: ajustes para implementacao de database

--- application/repository/artifact.go
+++ application/repository/artifact.go
@@ -10,35 +10,30 @@ type ArtifactRepositoryImpl struct {
 }
 
 func (a *ArtifactRepositoryImpl) Create(artifact domain.Artifact) (*domain.Artifact, error) {
-	result := a.Db.Create(artifact)
-	artifactResult := result.(domain.Artifact)
-	return &artifactResult, nil
+	var result domain.Artifact
+	a.Db.Create(artifact, &result)
+	return &result, nil
 }
 
 func (a *ArtifactRepositoryImpl) Save(artifact domain.Artifact) (*domain.Artifact, error) {
-	artifactSaved := a.Db.Save(artifact)
-	if artifactSaved == nil {
+	var artifactSaved domain.Artifact
+	a.Db.Save(artifact, &artifactSaved)
+	if &artifactSaved == nil || artifactSaved.ArtifactID == "" {
 		return nil, errors.New("ocorreu algum erro ao salvar artefato")
 	}
 
-	pointer := artifactSaved.(domain.Artifact)
-
-	return &pointer, nil
+	return &artifactSaved, nil
 }
 
 func (a *ArtifactRepositoryImpl) Get(searchParam map[string]string) ([]domain.Artifact, error) {
-	artifactsInterface := a.Db.Search(searchParam)
 	var artifacts []domain.Artifact
-
-	for _, ai := range artifactsInterface {
-		artifacts = append(artifacts, ai.(domain.Artifact))
-	}
+	a.Db.Search(searchParam, &artifacts)
 
 	return artifacts, nil
 }
 
 func (a *ArtifactRepositoryImpl) Delete(artifactId string) error {
-	wasDeleted := a.Db.Delete(map[string]string{"artifact_id": artifactId})
+	wasDeleted := a.Db.Delete(map[string]string{"artifact_id": artifactId}, domain.Artifact{})
 
 	if !wasDeleted {
 		return errors.New("ocorreu um erro ao deletar artefato da base de dados")
