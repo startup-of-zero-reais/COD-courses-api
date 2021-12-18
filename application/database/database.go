a09058f05156e3b0a4c031f159da81cb10c2d228
Jean <jean.carlosmolossi@gmail.com>
2021-12-18 00:34:00 -0300

    refactor: alterado modo de build de dsn e conexao

--- application/database/database.go
+++ application/database/database.go
@@ -13,13 +13,15 @@ import "gorm.io/gorm"
 type (
 	Database struct {
 		Db  *gorm.DB
+		Dsn string
 		Env string
 	}
 )
 
-func dsn(env string) string {
-	if env == "testing" {
-		return fmt.Sprintf("../test_db.sqlite")
+func (d *Database) BuildDsn() {
+	if d.Env == "testing" {
+		d.Dsn = fmt.Sprintf("./test_sqlite.db")
+		return
 	}
 
 	user := util.GetEnv("MYSQL_USER", "root")
@@ -28,30 +30,36 @@ func dsn(env string) string {
 	port := util.GetEnv("MYSQL_PORT", "3306")
 	dbase := util.GetEnv("MYSQL_DATABASE", "courses")
 
-	return fmt.Sprintf(
+	d.Dsn = fmt.Sprintf(
 		"%s:%s@tpc(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, pass, host, port, dbase,
 	)
 }
 
-func NewDatabase() *Database {
-	return &Database{
-		Env: "development",
+func NewDatabase(env ...string) *Database {
+	dbase := &Database{Env: "development"}
+
+	if len(env) > 0 {
+		dbase.Env = env[0]
 	}
+
+	dbase.BuildDsn()
+	return dbase
 }
 
 func (d *Database) Connect() {
 	if d.Env == "testing" {
-		db, err := gorm.Open(sqlite.Open(dsn(d.Env)))
+		db, err := gorm.Open(sqlite.Open(d.Dsn))
 		if err != nil {
-			log.Fatalf("erro ao iniciar conexao com banco: %s", err.Error())
+			fmt.Println(err.Error())
+			log.Fatalf("erro ao iniciar conexao com banco de teste: %s", err.Error())
 		}
 
 		d.Db = db
 		return
 	}
 
-	db, err := gorm.Open(mysql.Open(dsn(d.Env)))
+	db, err := gorm.Open(mysql.Open(d.Dsn))
 	if err != nil {
 		log.Fatalf("erro ao iniciar conexao com banco: %s", err.Error())
 	}
