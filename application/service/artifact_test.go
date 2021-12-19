0cc21fb97cc022e7b920f0bd1777c89b780bc3e9
Jean <jean.carlosmolossi@gmail.com>
2021-12-18 23:04:15 -0300

    test: refatorado teste de sucesso e adicionado teste de falha

--- application/service/artifact_test.go
+++ application/service/artifact_test.go
@@ -1,7 +1,9 @@
 package service_test
 
 import (
+	"errors"
 	"github.com/startup-of-zero-reais/COD-courses-api/application/service"
+	"github.com/startup-of-zero-reais/COD-courses-api/domain"
 	mocks "github.com/startup-of-zero-reais/COD-courses-api/mocks/domain"
 	"github.com/startup-of-zero-reais/COD-courses-api/tests/entity_mocks"
 	"github.com/stretchr/testify/require"
@@ -9,12 +11,18 @@ import (
 )
 
 func TestArtifactServiceAdd(t *testing.T) {
-	t.Run("should add artifact", func(t *testing.T) {
+	preAddTest := func(returns ...interface{}) (domain.ArtifactRepository, domain.Artifact) {
 		artifactSpy := *entity_mocks.ArtifactMock()
-		artifactDuble := entity_mocks.ArtifactMock("mock-uuid")
 
 		repo := new(mocks.ArtifactRepository)
-		repo.On("Create", artifactSpy).Return(artifactDuble, nil)
+		repo.On("Create", artifactSpy).Return(returns...)
+
+		return repo, artifactSpy
+	}
+
+	t.Run("should add artifact", func(t *testing.T) {
+		artifactDuble := entity_mocks.ArtifactMock("mock-uuid")
+		repo, artifactSpy := preAddTest(artifactDuble, nil)
 
 		svc := service.NewArtifactService(repo)
 		result, err := svc.Add(artifactSpy)
@@ -24,5 +32,14 @@ func TestArtifactServiceAdd(t *testing.T) {
 		require.Equal(t, artifactSpy.LessonID, result.LessonID)
 		require.Equal(t, artifactSpy.Link, result.Link)
 		require.NotNil(t, result.ArtifactID)
+	})
+	t.Run("should not add artifact and return error", func(t *testing.T) {
+		repo, artifactSpy := preAddTest(nil, errors.New("erro ao criar artefato"))
+
+		svc := service.NewArtifactService(repo)
+		result, err := svc.Add(artifactSpy)
+
+		require.Nil(t, result)
+		require.Error(t, err, "erro ao criar artefato")
 	})
 }
