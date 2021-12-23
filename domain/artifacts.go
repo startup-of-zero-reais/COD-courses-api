45615278b3718641e42786e81e269341e111cdb8
Jean <jean.carlosmolossi@gmail.com>
2021-12-22 23:13:04 -0300

    feat: adicionadas tags de json e paginator

--- domain/artifacts.go
+++ domain/artifacts.go
@@ -7,9 +7,9 @@ import (
 
 type (
 	Artifact struct {
-		ArtifactID string `gorm:"type:varchar(36);primaryKey;column:artifact_id;"`
-		LessonID   string `gorm:"type:varchar(36);column:lesson_id"`
-		Link       string `gorm:"type:varchar(255);column:link"`
+		ArtifactID string `json:"artifact_id" paginator:"key:artifact_id" gorm:"type:varchar(36);primaryKey;column:artifact_id;"`
+		LessonID   string `json:"lesson_id" paginator:"-" gorm:"type:varchar(36);column:lesson_id"`
+		Link       string `json:"_link" paginator:"_self" gorm:"type:varchar(255);column:link"`
 	}
 
 	ArtifactRepository interface {
