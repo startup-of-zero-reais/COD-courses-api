7527fa7c6f11d97031432c6ca2c5cf674f09f02e
Jean <jean.carlosmolossi@gmail.com>
2021-12-24 14:49:37 -0300

    feat: criado service de lesson

---
+++ application/service/lesson.go
@@ -0,0 +1,24 @@
+package service
+
+import "github.com/startup-of-zero-reais/COD-courses-api/domain"
+
+type (
+	LessonServiceImpl struct {
+	}
+)
+
+func (l *LessonServiceImpl) Add(lesson domain.Lesson) (*domain.Lesson, error) {
+	return nil, nil
+}
+
+func (l *LessonServiceImpl) ListBySection(sectionID string, query map[string]string) ([]domain.Lesson, error) {
+	return nil, nil
+}
+
+func (l *LessonServiceImpl) Get(lessonID string) (*domain.Lesson, error) {
+	return nil, nil
+}
+
+func (l *LessonServiceImpl) Delete(lessonID string) error {
+	return nil
+}
