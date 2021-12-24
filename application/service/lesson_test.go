cd603bdbfd6ff4189ef253ab6a608fe09e7e5c26
Jean <jean.carlosmolossi@gmail.com>
2021-12-24 15:12:58 -0300

    test: implementados testes de Delete

--- application/service/lesson_test.go
+++ application/service/lesson_test.go
@@ -1,6 +1,12 @@
 package service_test
 
-import "testing"
+import (
+	"errors"
+	"github.com/startup-of-zero-reais/COD-courses-api/application/service"
+	mocks "github.com/startup-of-zero-reais/COD-courses-api/mocks/domain"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestLessonServiceImpl_Add(t *testing.T) {}
 
@@ -10,4 +16,25 @@ func TestLessonServiceImpl_ListBySection(t *testing.T) {}
 
 func TestLessonServiceImpl_Get(t *testing.T) {}
 
-func TestLessonServiceImpl_Delete(t *testing.T) {}
+func TestLessonServiceImpl_Delete(t *testing.T) {
+	t.Run("should delete a lesson", func(t *testing.T) {
+		repo := new(mocks.LessonRepository)
+
+		repo.On("Delete", "lesson-id").Return(nil)
+
+		svc := service.NewLessonService(repo)
+		err := svc.Delete("lesson-id")
+
+		require.Nil(t, err)
+	})
+	t.Run("should fail on delete", func(t *testing.T) {
+		repo := new(mocks.LessonRepository)
+
+		repo.On("Delete", "lesson-id").Return(errors.New("erro ao deletar aula"))
+
+		svc := service.NewLessonService(repo)
+		err := svc.Delete("lesson-id")
+
+		require.EqualError(t, err, "erro ao deletar aula")
+	})
+}
