fb9825340cc59a22d9417fc9ae4b40e9f84d9ff5
Jean <jean.carlosmolossi@gmail.com>
2021-12-24 14:49:50 -0300

    test: criados testes de lesson service

---
+++ application/service/lesson_test.go
@@ -0,0 +1,11 @@
+package service_test
+
+import "testing"
+
+func TestLessonServiceImpl_Add(t *testing.T) {}
+
+func TestLessonServiceImpl_ListBySection(t *testing.T) {}
+
+func TestLessonServiceImpl_Get(t *testing.T) {}
+
+func TestLessonServiceImpl_Delete(t *testing.T) {}
