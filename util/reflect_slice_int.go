d5a54f044d9327498e5d325a523693c064bef485
Jean <jean.carlosmolossi@gmail.com>
2021-12-26 16:52:01 -0300

    feat: criado utilitário de reflexão

---
+++ util/reflect_slice_int.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"errors"
+	"reflect"
+)
+
+func ReflectSlice(thisSlice interface{}, ofThis interface{}) error {
+	ptrSlice := reflect.ValueOf(thisSlice)
+	reflected := reflect.ValueOf(ofThis)
+
+	if ptrSlice.Kind() != reflect.Ptr {
+		return errors.New("o primeiro argumento de ReflectSlice deve ser um ponteiro")
+	}
+
+	if reflected.Kind() != reflect.Slice {
+		return errors.New("o segundo argumento de ReflectSlice deve ser um slice")
+	}
+
+	reflectedSlice := reflect.MakeSlice(
+		reflect.SliceOf(reflected.Index(0).Type()),
+		0,
+		reflected.Len(),
+	)
+
+	for i := 0; i < reflected.Len(); i++ {
+		el := reflected.Index(i)
+
+		reflectedSlice = reflect.Append(reflectedSlice, el)
+	}
+
+	ptrSlice.Elem().Set(reflectedSlice)
+
+	return nil
+}
