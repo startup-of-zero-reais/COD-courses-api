48097511afd55046d87440973afdf3de215b4811
Jean <jean.carlosmolossi@gmail.com>
2021-12-26 14:21:21 -0300

    test: iniciados testes de section

---
+++ application/repository/section_test.go
@@ -0,0 +1,23 @@
+package repository_test
+
+import "testing"
+
+func TestSectionRepositoryImpl_Create(t *testing.T) {
+
+}
+
+func TestSectionRepositoryImpl_Save(t *testing.T) {
+
+}
+
+func TestSectionRepositoryImpl_Get(t *testing.T) {
+
+}
+
+func TestSectionRepositoryImpl_Delete(t *testing.T) {
+
+}
+
+func TestSectionRepositoryImpl_Count(t *testing.T) {
+	
+}
