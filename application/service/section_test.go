53acd6f8c2b61eb26671c33447dfc402ec4f662c
Jean <jean.carlosmolossi@gmail.com>
2021-12-26 22:00:38 -0300

    test: criados test cases de Service

---
+++ application/service/section_test.go
@@ -0,0 +1,51 @@
+package service_test
+
+import "testing"
+
+func TestSectionServiceImpl_Add(t *testing.T) {
+	t.Run("should add a section", func(t *testing.T) {
+
+	})
+	t.Run("should fail on add a section", func(t *testing.T) {
+
+	})
+}
+
+func TestSectionServiceImpl_Get(t *testing.T) {
+	t.Run("should get single section", func(t *testing.T) {
+
+	})
+	t.Run("should fail if has no section", func(t *testing.T) {
+
+	})
+	t.Run("should fail if has more than one section", func(t *testing.T) {
+
+	})
+}
+
+func TestSectionServiceImpl_ListByModule(t *testing.T) {
+	t.Run("should list sections", func(t *testing.T) {
+
+	})
+	t.Run("should return empty slice if has no sections", func(t *testing.T) {
+
+	})
+}
+
+func TestSectionServiceImpl_Save(t *testing.T) {
+	t.Run("should update a section", func(t *testing.T) {
+
+	})
+	t.Run("should fail if does not exists the section was updating", func(t *testing.T) {
+
+	})
+}
+
+func TestSectionServiceImpl_Delete(t *testing.T) {
+	t.Run("should delete a section", func(t *testing.T) {
+
+	})
+	t.Run("should fail if no delete", func(t *testing.T) {
+
+	})
+}
