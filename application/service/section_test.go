59edc0ed077a0b9a4ae1766672d87f52393d7b24
Jean <jean.carlosmolossi@gmail.com>
2021-12-26 22:29:39 -0300

    test: criado test suit e primeiro teste

--- application/service/section_test.go
+++ application/service/section_test.go
@@ -1,10 +1,74 @@
 package service_test
 
-import "testing"
+import (
+	"github.com/startup-of-zero-reais/COD-courses-api/application/service"
+	"github.com/startup-of-zero-reais/COD-courses-api/domain"
+	mocks "github.com/startup-of-zero-reais/COD-courses-api/mocks/domain"
+	"github.com/startup-of-zero-reais/COD-courses-api/tests/entity_mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type TestSuit struct {
+	Meth          string
+	Repo          *mocks.SectionRepository
+	Args          []interface{}
+	MockExecution []func(args mock.Arguments)
+}
+
+func BeforeAllSection(meth string) *TestSuit {
+	return &TestSuit{
+		Meth: meth,
+		Repo: new(mocks.SectionRepository),
+	}
+}
+
+func (t *TestSuit) BeforeEach(args ...interface{}) {
+	t.Args = args
+}
+
+func (t *TestSuit) AfterEach() {
+	t.Args = nil
+}
+
+func (t *TestSuit) MockReturns(returns ...interface{}) {
+	t.Repo.On(t.Meth, t.Args...).Return(returns...).Run(func(args mock.Arguments) {
+		if len(t.MockExecution) > 0 {
+			for _, override := range t.MockExecution {
+				override(args)
+			}
+		}
+	})
+}
+
+func (t *TestSuit) PutExecutionMock(m func(args mock.Arguments)) *TestSuit {
+	t.MockExecution = append(t.MockExecution, m)
+	return t
+}
+
+func (t *TestSuit) getService() domain.SectionService {
+	return service.NewSectionService(t.Repo)
+}
 
 func TestSectionServiceImpl_Add(t *testing.T) {
-	t.Run("should add a section", func(t *testing.T) {
+	ts := BeforeAllSection("Create")
 
+	t.Run("should add a section", func(t *testing.T) {
+		sectionSpy := *entity_mocks.SectionMock(map[string]interface{}{
+			"section_id": "-",
+		})
+
+		ts.BeforeEach(sectionSpy)
+		defer ts.AfterEach()
+
+		ts.MockReturns(&sectionSpy, nil)
+		svc := ts.getService()
+
+		expected, err := svc.Add(sectionSpy)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, expected)
 	})
 	t.Run("should fail on add a section", func(t *testing.T) {
 
