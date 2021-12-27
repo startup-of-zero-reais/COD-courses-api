2e958bc0da5058e4992784391c88525cc875014f
Jean <jean.carlosmolossi@gmail.com>
2021-12-26 23:00:35 -0300

    test: ajustada paginacao de test

--- application/service/section_test.go
+++ application/service/section_test.go
@@ -107,7 +107,7 @@ func TestSectionServiceImpl_Get(t *testing.T) {
 		}
 		pagination := map[string]string{
 			"page":     "1",
-			"per_page": "10",
+			"per_page": "1",
 		}
 
 		ts.BeforeEach(search, pagination)
